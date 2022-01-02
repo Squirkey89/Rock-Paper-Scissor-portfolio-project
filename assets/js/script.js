25642e7740e933d55879b17b4edd4d912455d594
Squirkey89 <squirke89@gmail.com>
2022-01-02 16:53:35 +0000

    Add margin to images. Set up Dom nanipulators and functions.

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const compScoreDisplayed = document.getElementById('computer-score');
+const userScoreDisplayed = document.getElementById('user-score');
+const usersPick = document.getElementById('user');
+const computersPick = document.getElementById('computer');
+const results = document.getElementById('result')
+
+
+
+function generateComputerChoice() {
+    
+}
+
+function getUserChoice(){
+
+}
+
+function mainGame() {
+}
+
+function getResult(){
+   
+}
+
+function incrementUseScore() {
+}
+
+
+function incrementComputerScore() {}
