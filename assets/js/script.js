1ab1eba9b02f52ca08ebda73a318fcb810a9cfba
Squirkey89 <squirke89@gmail.com>
2022-01-03 13:19:34 +0000

    Add a score function to calculate who won and how to tally the score correctly. Also added further to HTML structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const compScoreDisplayed = document.getElementById('computer-score');
 const userScoreDisplayed = document.getElementById('user-score');
+const message = document.getElementById('message')
 const results = document.getElementById('result')
 const buttons = document.getElementsByTagName("button");
 
@@ -24,25 +25,51 @@ function generateComputerChoice() {
 function mainGame(userChoice) {
 	let computerChoice = generateComputerChoice()
 	if (computerChoice === 'rock' && userChoice === 'paper') {
-		document.getElementById('result').innerHTML = "You chose paper. Computer chose rock"
+		document.getElementById('message').innerHTML = "You choose paper. Computer choose rock"
+		userWin()
 	}
 	if (computerChoice === 'paper' && userChoice === 'scissors') {
-		document.getElementById('result').innerHTML = "You chose paper. Computer chose rock"
+		document.getElementById('message').innerHTML = "You choose scissors. Computer choose rock"
+		userWin()
 	}
 	if (computerChoice === 'scissors' && userChoice === 'rock') {
-		document.getElementById('result').innerHTML = "You chose rock. Computer chose scissors"
+		document.getElementById('message').innerHTML = "You choose rock. Computer choose scissors"
+		userWin()
 	}
 	if (computerChoice === 'paper' && userChoice === 'rock') {
-		document.getElementById('result').innerHTML = "You chose rock. Computer chose paper"
+		document.getElementById('message').innerHTML = "You choose rock. Computer choose paper"
+		computerWin()
 	}
 	if (computerChoice === 'scissors' && userChoice === 'paper') {
-		document.getElementById('result').innerHTML = "You chose paper. Computer chose scissors"
+		document.getElementById('message').innerHTML = "You choose paper. Computer choose scissors"
+		computerWin()
 	}
 	if (computerChoice === 'rock' && userChoice === 'scissors') {
-		document.getElementById('result').innerHTML = "You chose scissors. Computer chose rock"
+		document.getElementById('message').innerHTML = "You choose scissors. Computer choose rock"
+		computerWin()
 	}
 	else if(computerChoice === userChoice)
-		{document.getElementById('result').innerHTML = "Its a tie game!"
+		{document.getElementById('message').innerHTML = "Its a tie game!"
 	}
 	
 }
+function userWin(){
+	let oldScore = parseInt(document.getElementById("user-score").innerText);
+	document.getElementById("user-score").innerText = ++oldScore;	
+	document.getElementById("result").innerHTML='You win!'
+}
+function computerWin(){
+	let oldScore = parseInt(document.getElementById("computer-score").innerText);
+	document.getElementById("computer-score").innerText = ++oldScore;	
+	document.getElementById("result").innerHTML='You Lose'
+
+}
+let result
+
+function drawGame(){
+	 if(computerChoice === userChoice){
+		result.style.display(hidden)
+	}}
+
+result.addEventListener('click', function() {
+	drawGame()})
